3bf8aadd625e5e0ff737a60f0b49e57a85ef432d
Soul244 <hasancangedik@hotmail.com>
2019-02-15 02:48:12 +0300

    (hotfix) message pop-up modules removed

--- src/main.js
+++ src/main.js
@@ -3,20 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./vuex/store";
 import * as VueGoogleMaps from 'vue2-google-maps'
-import FlashMessage from '@smartweb/vue-flash-message';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'vue-flash-message/dist/vue-flash-message.min.css';
 import moment from 'moment'
-import VueFlashMessage from 'vue-flash-message';
 Vue.prototype.moment = moment
 
 Vue.use(BootstrapVue);
-Vue.use(FlashMessage, {
-  time: 3000,
-});
-Vue.use(VueFlashMessage);
 
 Vue.use(VueGoogleMaps, {
   load: {
